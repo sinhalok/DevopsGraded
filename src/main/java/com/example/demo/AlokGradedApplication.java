8a00e3a11c0585942d4b204cf882bf7e40748326
ALOK SINHA <35992268+sinhalok@users.noreply.github.com>
2021-12-12 07:48:01 +0530

    Add files via upload

---
+++ src/main/java/com/example/demo/AlokGradedApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlokGradedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlokGradedApplication.class, args);
+		System.out.println("Hello Dev-Ops");
+	}
+
+}
